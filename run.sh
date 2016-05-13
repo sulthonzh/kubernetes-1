e3ca015c240762230e5f52de7a3892ac43323ae5
Asim <asim@chuhnk.me>
2016-05-13 22:28:43 +0100

    Inform the database install

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 cmd=$1
 dir=$2
 kube=kubectl
-list=( registry micro misc platform )
+list=( database registry micro misc platform )
 
 start() {
 	if [ -z $dir ]; then
@@ -18,7 +18,12 @@ start() {
 
 	find $dir -name "*.yaml" | while read file; do
 		$kube create -f $file
-	done	
+	done
+
+	if [ -z $dir ] || [ "$dir" == "database" ]; then
+		node=`kubectl get pods | grep pxc-node3 | awk '{print $1}'`
+		echo "Run \"kubectl exec $node -i -t -- mysql -u root -p -h pxc-cluster\" and install DBs in database/platform.sql"
+	fi
 }
 
 stop() {
