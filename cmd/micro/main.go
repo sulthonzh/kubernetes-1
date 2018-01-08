9d7f8b8ccb80826d04cb630893c7eab8d7bafad6
Asim Aslam <asim@chuhnk.me>
2018-01-08 11:11:14 +0000

    default to grpc

--- cmd/micro/main.go
+++ cmd/micro/main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"os"
 
+	"github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/server"
+	cli "github.com/micro/go-plugins/client/grpc"
 	_ "github.com/micro/go-plugins/registry/kubernetes"
+	srv "github.com/micro/go-plugins/server/grpc"
 	"github.com/micro/micro/cmd"
 )
 
 func init() {
 	os.Setenv("MICRO_REGISTRY", "kubernetes")
+	client.DefaultClient = cli.NewClient()
+	server.DefaultServer = srv.NewServer()
 }
 
 func main() {
