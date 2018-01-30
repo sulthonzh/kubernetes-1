8de989ec137ec17ce45e59e342bad47debd9a285
Asim Aslam <asim@chuhnk.me>
2018-01-30 11:40:03 +0000

    add greeter code

---
+++ examples/greeter/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	hello "github.com/micro/examples/greeter/srv/proto/hello"
+	"github.com/micro/go-micro"
+	k8s "github.com/micro/kubernetes/go/micro"
+
+	"golang.org/x/net/context"
+)
+
+type Say struct{}
+
+func (s *Say) Hello(ctx context.Context, req *hello.Request, rsp *hello.Response) error {
+	log.Print("Received Say.Hello request")
+	rsp.Msg = "Hello " + req.Name
+	return nil
+}
+
+func main() {
+	service := k8s.NewService(
+		micro.Name("go.micro.srv.greeter"),
+		micro.RegisterTTL(time.Second*30),
+		micro.RegisterInterval(time.Second*10),
+	)
+
+	service.Init()
+
+	hello.RegisterSayHandler(service.Server(), new(Say))
+
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
