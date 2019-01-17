afd8fccebc27cb2a19e00ad90a3c5f116db5dd33
Asim Aslam <asim@aslam.me>
2019-01-17 08:52:34 +0000

    update web

--- go/web/web.go
+++ go/web/web.go
@@ -1,10 +1,9 @@
 package web
 
 import (
+	"github.com/micro/go-grpc"
 	"github.com/micro/go-micro"
-	cli "github.com/micro/go-plugins/client/grpc"
 	"github.com/micro/go-plugins/registry/kubernetes"
-	srv "github.com/micro/go-plugins/server/grpc"
 	"github.com/micro/go-web"
 
 	// static selector offloads load balancing to k8s services
@@ -16,15 +15,11 @@ import (
 // NewService returns a web service for kubernetes
 func NewService(opts ...web.Option) web.Service {
 	// setup
-	c := cli.NewClient()
-	s := srv.NewServer()
 	k := kubernetes.NewRegistry()
 	st := static.NewSelector()
 
 	// create new service
-	service := micro.NewService(
-		micro.Server(s),
-		micro.Client(c),
+	service := grpc.NewService(
 		micro.Registry(k),
 		micro.Selector(st),
 	)
